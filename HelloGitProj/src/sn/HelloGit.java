4c7689b795866bc28382c25b6f6b5bac8dbf47f2
snewstadt <dart_girl@yahoo.com>
2017-10-28 13:43:12 -0500

    First commit from Eclipse project.

---
+++ HelloGitProj/src/sn/HelloGit.java
@@ -0,0 +1,22 @@
+/** HelloGitProj
+* 
+*
+*
+* @author Sandra-local, Oct 28, 2017
+*
+**/
+package sn;
+
+/**
+ * @author Sandra Newstadt 10/28/2017
+ * Testing using Eclispe with GitHub
+ *
+ */
+public class HelloGit {
+
+
+	public static void main(String[] args) {
+		System.out.println("Hello Github!");
+	}
+
+}
